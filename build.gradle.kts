df7027247d0cef6efe0c2710eabbc985bf262746
kannna5296 <kannnahacks5296@gmail.com>
2025-04-19 13:55:11 +0900

    jooqで作成OK

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
 	kotlin("plugin.spring") version "1.9.25"
 	id("org.springframework.boot") version "3.4.4"
 	id("io.spring.dependency-management") version "1.1.7"
+	id("nu.studer.jooq") version "8.2"
 }
 
 group = "com.example"
@@ -24,6 +25,12 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	implementation("org.springframework.boot:spring-boot-starter-jooq")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("mysql:mysql-connector-java:8.0.33")
+
+	jooqGenerator("mysql:mysql-connector-java:8.0.33")
 }
 
 kotlin {
@@ -35,3 +42,34 @@ kotlin {
 tasks.withType<Test> {
 	useJUnitPlatform()
 }
+
+
+jooq {
+	version.set("3.18.5")
+	configurations {
+		create("main") { // ←ここが「named」ではなく「create」！
+			jooqConfiguration.apply {
+				jdbc.apply {
+					driver = "com.mysql.cj.jdbc.Driver"
+					url = "jdbc:mysql://localhost:3306/todo_db"
+					user = "user"
+					password = "password"
+				}
+				generator.apply {
+					name = "org.jooq.codegen.DefaultGenerator"
+					database.apply {
+						name = "org.jooq.meta.mysql.MySQLDatabase"
+						inputSchema = "todo_db"
+						includes = "todos"
+					}
+					target.apply {
+						packageName = "com.example.todo.jooq.generated"
+						directory = "src/main/generated"
+					}
+				}
+			}
+		}
+	}
+}
+
+sourceSets["main"].java.srcDir("src/main/generated")
